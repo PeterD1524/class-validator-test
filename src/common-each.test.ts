367f6df721d5575b07fbf6e38a9f25cf17dc3025
PeterD1524 <qaz246135@gmail.com>
2025-06-11 03:39:08 +0800

    test Equals each

--- src/common-each.test.ts
+++ src/common-each.test.ts
@@ -1,6 +1,6 @@
 import { validate, ValidationError } from "class-validator";
 import { describe, expect, it } from "vitest";
-import { TestIsDefinedEach } from "./common-each.js";
+import { TestEqualsEach, TestIsDefinedEach } from "./common-each.js";
 
 describe("IsDefined each", () => {
   it("undefined", async () => {
@@ -60,6 +60,79 @@ describe("IsDefined each", () => {
   });
   it("new Map([[0, undefined]])", async () => {
     const t = new TestIsDefinedEach();
+    t.value = new Map([[0, undefined]]);
+    expect(await validate(t)).toStrictEqual([expect.any(ValidationError)]);
+  });
+});
+
+describe("Equals each", () => {
+  it("undefined", async () => {
+    const t = new TestEqualsEach();
+    expect(await validate(t)).toStrictEqual([expect.any(ValidationError)]);
+  });
+  it("0", async () => {
+    const t = new TestEqualsEach();
+    t.value = 0;
+    expect(await validate(t)).toStrictEqual([]);
+  });
+  it("[]", async () => {
+    const t = new TestEqualsEach();
+    t.value = [];
+    expect(await validate(t)).toStrictEqual([]);
+  });
+  it("[0]", async () => {
+    const t = new TestEqualsEach();
+    t.value = [0];
+    expect(await validate(t)).toStrictEqual([]);
+  });
+  it("[undefined]", async () => {
+    const t = new TestEqualsEach();
+    t.value = [undefined];
+    expect(await validate(t)).toStrictEqual([expect.any(ValidationError)]);
+  });
+  it("[[undefined]]", async () => {
+    const t = new TestEqualsEach();
+    t.value = [[undefined]];
+    expect(await validate(t)).toStrictEqual([expect.any(ValidationError)]);
+  });
+  it("{}", async () => {
+    const t = new TestEqualsEach();
+    t.value = {};
+    expect(await validate(t)).toStrictEqual([expect.any(ValidationError)]);
+  });
+  it("{ 0: undefined }", async () => {
+    const t = new TestEqualsEach();
+    t.value = { 0: undefined };
+    expect(await validate(t)).toStrictEqual([expect.any(ValidationError)]);
+  });
+  it("{ undefined: 0 }", async () => {
+    const t = new TestEqualsEach();
+    t.value = { undefined: 0 };
+    expect(await validate(t)).toStrictEqual([expect.any(ValidationError)]);
+  });
+  it("sparse array", async () => {
+    const t = new TestEqualsEach();
+    // eslint-disable-next-line no-sparse-arrays
+    t.value = [,];
+    expect(await validate(t)).toStrictEqual([]);
+  });
+  it("new Set([0])", async () => {
+    const t = new TestEqualsEach();
+    t.value = new Set([0]);
+    expect(await validate(t)).toStrictEqual([]);
+  });
+  it("new Set([undefined])", async () => {
+    const t = new TestEqualsEach();
+    t.value = new Set([undefined]);
+    expect(await validate(t)).toStrictEqual([expect.any(ValidationError)]);
+  });
+  it("new Map([[undefined, 0]])", async () => {
+    const t = new TestEqualsEach();
+    t.value = new Map([[undefined, 0]]);
+    expect(await validate(t)).toStrictEqual([]);
+  });
+  it("new Map([[0, undefined]])", async () => {
+    const t = new TestEqualsEach();
     t.value = new Map([[0, undefined]]);
     expect(await validate(t)).toStrictEqual([expect.any(ValidationError)]);
   });
--- src/common-each.ts
+++ src/common-each.ts
@@ -1,6 +1,11 @@
-import { IsDefined } from "class-validator";
+import { Equals, IsDefined } from "class-validator";
 
 export class TestIsDefinedEach {
   @IsDefined({ each: true })
+  value: unknown;
+}
+
+export class TestEqualsEach {
+  @Equals(0, { each: true })
   value: unknown;
 }
